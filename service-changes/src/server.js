984af768aedeab4b657f15a78b2d3643344780f7
Ian Huntington <ihuntington85@gmail.com>
2020-06-07 22:24:38 +0100

    Refactor changes service to use new API routes

--- service-changes/src/server.js
+++ service-changes/src/server.js
@@ -9,106 +9,93 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const Hapi = require('@hapi/hapi');
-const db = require('./db');
+const Wreck = require('@hapi/wreck');
 const Spotify = require('./spotify');
 
 async function getRecentlyPlayed(user) {
     const spotify = new Spotify(user.token, user.refresh_token);
-    const after = user.recently_played_at && user.recently_played_at.getTime() || null;
-    const output = {
-        user
-    };
-
-    try {
-        const { tokens, data } = await spotify.recentlyPlayed(after);
-
-        output.user.token = tokens.token;
-        output.user.refresh_token = tokens.refreshToken;
-
-        output.tracks = data.items.map((item) => ({
-            artists: item.track.artists,
-            duration_ms: item.track.duration_ms,
-            id: item.track.id,
-            name: item.track.name,
-            played_at: item.played_at,
-        }));
-    } catch (err) {
-        console.log(err);
-        output.error = err.message;
-    }
-
-    return new Promise((resolve) => resolve(output));
-}
-
-async function* importTracks(uid, items) {
-    for (const item of items) {
-        const output = {};
-
-        try {
-            const data = await db.importTrack(uid, item);
-            output.status = 'success';
-            output.data = data;
-        } catch (err) {
-            output.status = 'error';
-            output.message = err.message;
-        }
-
-        yield output;
-    }
+    const after = user.recently_played_at && new Date(user.recently_played_at).getTime() || null;
+    const { tokens, data } = await spotify.recentlyPlayed(after);
+    return Promise.resolve({ tokens, tracks: data });
 }
 
 async function* makeRecentlyPlayedGenerator(users) {
     for (const user of users) {
-        const recentlyPlayed = await getRecentlyPlayed(user);
-        const { id: uid, token, refresh_token } = recentlyPlayed.user;
-
-        await db.updateUserTokens(uid, token, refresh_token);
-
-        yield* importTracks(uid, recentlyPlayed.tracks);
+        try {
+            const { tokens, tracks }  = await getRecentlyPlayed(user);
+            yield {
+                user,
+                tokens: {
+                    token: tokens.token,
+                    refresh_token: tokens.refreshToken,
+                },
+                tracks: tracks.items,
+            };
+        } catch (e) {
+            console.error(e);
+            yield {
+                user,
+                tokens: { token: null, refresh_token: null },
+                tracks: [],
+            };
+        }
     }
 }
 
 const server = Hapi.server({
     host: '0.0.0.0',
-    port: process.env.PORT || 3010,
+    port: process.env.PORT || 6000,
 });
 
 server.route({
     method: 'GET',
-    path: '/recently-played',
+    path: '/',
     handler: async (request, h) => {
-        if (
-            process.env.NODE_ENV === 'production' &&
-            !request.headers['x-appengine-cron']
-        ) {
-            return h.redirect('https://www.haveiplayedbowie.today');
-        }
-
         let users;
 
         try {
-            users = await db.getUsersByRecentlyPlayed();
+            users = await Wreck.get('http://localhost:5000/users/recently-played', { json: true });
         } catch (err) {
-            console.log('Cannot fetch users');
-            console.log(err.stack);
+            console.log('Cannot fetch users by recently played');
+
             return h.response().code(500);
         }
 
-        const recentlyPlayedGenerator = makeRecentlyPlayedGenerator(users);
+        if (users.payload.users.length === 0) {
+            return { scrobbles: [] };
+        }
+
+        const recentlyPlayedIterator = makeRecentlyPlayedGenerator(users.payload.users);
         const data = [];
 
-        // TODO: I feel like there is a better way to do this
-        for await (const result of recentlyPlayedGenerator) {
-            console.log('Import tracks status', result.status);
-            if (result.status === 'success') {
-                data.push(result.data);
-            } else {
-                console.log(result.message);
+        for await (const { user, tokens, tracks } of recentlyPlayedIterator) {
+            if (user.token !== tokens.token || user.refresh_token !== tokens.refresh_token) {
+                try {
+                    await Wreck.patch(`http://localhost:5000/users/${user.id}`, {
+                        payload: { ...tokens },
+                    });
+                } catch (e) {
+                    console.log('Unable to update tokens for user', user.id);
+                }
+            }
+
+            try {
+                const response = await Wreck.post('http://localhost:5000/scrobbles', {
+                    payload: { user: user.id, items: tracks.items },
+                    json: true,
+                });
+
+                data.push({
+                    user: user.id,
+                    scrobbles: response.payload.scrobbles,
+                });
+            } catch (e) {
+                console.log('Unable to add scrobbles for user', user.id);
             }
         }
 
         if (process.env.NODE_ENV === 'development') {
-            return h.response({ data }).code(200);
+            return h.response({ scrobbles: data }).code(200);
         }
 
         return h.response().code(200);
@@ -127,6 +114,7 @@ exports.start = async () => {
 };
 
 process.on('unhandledRejection', (err) => {
+    console.log('Unhandled rejection');
     console.log(err);
     // TODO: check how this will be handled on GAE
     process.exit(1);
