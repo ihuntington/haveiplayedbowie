d2502011eae0aa8f5c085725381b699c153a45c3
Ian Huntington <ihuntington85@gmail.com>
2020-07-01 22:33:41 +0100

    Remove join on artists_tracks to remove duplicate scrobbles in API response

--- service-api/src/sql/scrobbles/find-by-user-and-date.sql
+++ service-api/src/sql/scrobbles/find-by-user-and-date.sql
@@ -1,7 +1,6 @@
 SELECT s.id, s.played_at, t.id AS track_id, t.name AS track_name, t.duration_ms AS track_duration, t.spotify_id
 FROM scrobbles s
 JOIN tracks t ON t.id = s.track_id
-JOIN artists_tracks artr ON artr.track_id = t.id
 WHERE s.user_id = ${uid}
 AND CAST(s.played_at AS DATE) = ${date}
 ORDER BY s.played_at ASC;
