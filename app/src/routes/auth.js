16e2bfec52a2bb80f37de449f84061b96617312b
Ian Huntington <ihuntington85@gmail.com>
2020-08-01 14:38:47 +0100

    Fix /auth route in App for creating a new user

--- app/src/routes/auth.js
+++ app/src/routes/auth.js
@@ -8,24 +8,34 @@ async function spotifyAuth(request, h) {
         return `Authentication failed due to: ${request.auth.error.message}`;
     }
 
-    try {
-        const { credentials } = request.auth;
+    const { credentials } = request.auth;
+    let user = null;
 
-        const response = await Wreck.get(`${process.env.SERVICE_API_URL}/users?email=${credentials.profile.email}`, {
+    try {
+        const { payload } = await Wreck.get(`${process.env.SERVICE_API_URL}/users?email=${credentials.profile.email}`, {
             json: true,
         });
 
-        let user = response.payload;
+        user = payload;
+    } catch (err) {
+        if (e.data.payload.statusCode === 400) {
+            console.log('/auth could not match email address with existing user');
+        } else {
+            // TODO: server error
+            console.error(err);
+            return h.redirect('/');
+        }
+    }
 
-        // TODO: even if user exists, may want to update profile in case any changes
-        if (!user) {
+    if (!user) {
+        try {
+            // TODO: even if user exists, may want to update profile in case any changes
             const profile = {
                 displayName: credentials.profile.displayName,
                 id: credentials.profile.id,
                 username: credentials.profile.username,
             };
-
-            const newUserResponse = await Wreck.post(`${process.env.SERVICE_API_URL}/users`, {
+            const { payload } = await Wreck.post(`${process.env.SERVICE_API_URL}/users`, {
                 payload: {
                     email: credentials.profile.email,
                     token: credentials.token,
@@ -35,21 +45,22 @@ async function spotifyAuth(request, h) {
                 json: true,
             });
 
-            user = newUserResponse.payload;
+            user = payload;
+        } catch (err) {
+            // TODO: Unable to create user
+            console.log('/auth could not create new user');
+            console.error(err);
+            h.redirect('/login');
         }
-
-        request.cookieAuth.set({ id: user.id });
-
-        if (!user.username) {
-            return h.redirect('/account/username');
-        }
-
-        return h.redirect('/');
-    } catch (err) {
-        console.log(err);
-
-        return h.redirect('/login');
     }
+
+    request.cookieAuth.set({ id: user.id });
+
+    if (!user.username) {
+        return h.redirect('/account/username');
+    }
+
+    return h.redirect('/');
 }
 
 function login(request, h) {
