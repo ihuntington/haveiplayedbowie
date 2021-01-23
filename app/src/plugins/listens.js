3dd43aee330e59639639fc11932ab2afb7de1f13
Ian Huntington <ihuntington85@gmail.com>
2021-01-23 17:20:40 +0000

    App: Move listens back to plugin

--- app/src/plugins/listens.js
+++ app/src/plugins/listens.js
@@ -7,6 +7,58 @@ const plugin = {
     name: 'app/listens',
     dependencies: ['@hapi/cookie'],
     register: async (server) => {
+        server.route({
+            method: 'POST',
+            path: '/api/listens',
+            options: {
+                auth: {
+                    strategy: 'session',
+                    mode: 'required',
+                },
+                plugins: {
+                    'hapi-auth-cookie': {
+                        // disable redirect to /login for an API route
+                        redirectTo: false,
+                    },
+                },
+                handler: async (request, h) => {
+                    // return request.payload;
+                    const { played_at, segment }  = request.payload;
+                    const spotify = new Spotify();
+
+                    let trackId = null;
+                    let track = null;
+
+                    if (segment.uris && segment.uris.length) {
+                        const service = segment.uris.find((service) => {
+                            return service.id === "commercial-music-service-spotify";
+                        });
+
+                        if (service) {
+                            trackId = Spotify.extractTrackId(service.uri);
+                        }
+                    }
+
+                    if (trackId) {
+                        track =  await spotify.getTrackById(trackId);
+                    }
+
+                    // TODO if not found on spotify
+                    if (track) {
+                        const user = request.auth.credentials.id;
+                        const listen = await services.scrobbles.add(user, played_at, track);
+
+                        if (listen) {
+                            return h.response().code(201);
+                        }
+                    }
+                },
+                cors: {
+                    origin: [process.env.APP_CORS_ORIGIN],
+                    credentials: true,
+                },
+            },
+        });
     },
 };
 
--- app/src/server.js
+++ app/src/server.js
@@ -6,8 +6,6 @@ const Hapi = require('@hapi/hapi');
 const Boom = require('@hapi/boom');
 
 const plugins = require('./plugins');
-const services = require('./services');
-const Spotify = require('./library/spotify');
 const { sessionValidator } = require('./validators/session');
 
 let server = null;
@@ -67,7 +65,7 @@ const setup = async () => {
     server.register([plugins.auth]);
     server.register([plugins.charts]);
     server.register([plugins.home]);
-    // server.register([plugins.listens]);
+    server.register([plugins.listens]);
     server.register([plugins.tracks]);
     server.register([plugins.users]);
 
@@ -117,60 +115,6 @@ const setup = async () => {
             },
         },
     });
-
-    server.route({
-        method: 'POST',
-        path: '/api/listens',
-        options: {
-            auth: {
-                strategy: 'session',
-                mode: 'required',
-            },
-            plugins: {
-                'hapi-auth-cookie': {
-                    // disable redirect to /login for an API route
-                    redirectTo: false,
-                },
-            },
-            handler: async (request, h) => {
-                // return request.payload;
-                const { played_at, segment }  = request.payload;
-                const spotify = new Spotify();
-
-                let trackId = null;
-                let track = null;
-
-                if (segment.uris && segment.uris.length) {
-                    const service = segment.uris.find((service) => {
-                        return service.id === "commercial-music-service-spotify";
-                    });
-
-                    if (service) {
-                        trackId = Spotify.extractTrackId(service.uri);
-                    }
-                }
-
-                if (trackId) {
-                    track =  await spotify.getTrackById(trackId);
-                }
-
-                // TODO if not found on spotify
-                if (track) {
-                    const user = request.auth.credentials.id;
-                    const listen = await services.scrobbles.add(user, played_at, track);
-
-                    if (listen) {
-                        return h.response().code(201);
-                    }
-                }
-            },
-            cors: {
-                origin: [process.env.APP_CORS_ORIGIN],
-                credentials: true,
-            },
-        },
-    });
-
 
     return server;
 };
