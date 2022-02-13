6a666b8a28c15931762a7da77e8d509bc8129f45
Ian Huntington <ihuntington85@gmail.com>
2022-02-13 17:58:20 +0000

    fix: rename seek to SonarSeekEvent for embedded player events

--- app/src/bookmarklet/__snowpack__/env.js
+++ app/src/bookmarklet/__snowpack__/env.js
@@ -1,1 +1,7 @@
-export default {"SNOWPACK_PUBLIC_BOWIE_URL":"https://www.haveiplayedbowie.today","SNOWPACK_PUBLIC_BBC_URL":"https://www.bbc.co.uk","MODE":"production","NODE_ENV":"production","SSR":false};
+export default {
+    SNOWPACK_PUBLIC_BOWIE_URL: "https://www.bowie.test",
+    SNOWPACK_PUBLIC_BBC_URL: "https://www.bbc.co.uk",
+    MODE: "production",
+    NODE_ENV: "production",
+    SSR: false,
+};
