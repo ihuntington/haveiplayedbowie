aca8d3643f8e6ea596fed2946203e7957a4d2031
Ian Huntington <ihuntington85@gmail.com>
2022-02-19 14:20:50 +0000

    APP: update URL in bookmarklet and snowpack env

--- app/src/bookmarklet/__snowpack__/env.js
+++ app/src/bookmarklet/__snowpack__/env.js
@@ -1,5 +1,5 @@
 export default {
-    SNOWPACK_PUBLIC_BOWIE_URL: "https://www.bowie.test",
+    SNOWPACK_PUBLIC_BOWIE_URL: "https://www.haveiplayedbowie.today",
     SNOWPACK_PUBLIC_BBC_URL: "https://www.bbc.co.uk",
     MODE: "production",
     NODE_ENV: "production",
