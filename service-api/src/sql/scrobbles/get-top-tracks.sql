07155449954c2eff22197291576d8cc6eb807a38
Ian Huntington <ian.huntington@plumguide.com>
2022-10-12 12:48:27 +0100

    chore: add spotify_id to top tracks SQL select

--- service-api/src/sql/scrobbles/get-top-tracks.sql
+++ service-api/src/sql/scrobbles/get-top-tracks.sql
@@ -1,4 +1,4 @@
-SELECT tracks.id AS id, tracks.name AS name, COUNT(scrobbles.track_id) AS total
+SELECT tracks.id AS id, tracks.name AS name, tracks.spotify_id AS spotify_id, COUNT(scrobbles.track_id) AS total
 FROM scrobbles
 JOIN tracks ON tracks.id = scrobbles.track_id
 $(join:raw)
